64608b0dfd5c4a182e4a16c5592884cc8887134f
naitotaisuke1013 <naitotaisuke1013@gmail.com>
2024-10-23 17:07:25 +0900

    [update]

--- test.sh
+++ test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-
 echo "-------------------------------"
-
 
 # 圧縮するファイルのパス
 file_to_zip="./input.txt"
 
 # 出力するZIPファイルのパス
-zip_file_path="./output.txt"
-
-
+zip_file_path="./output.zip"  # 拡張子を .zip に変更
 
 # ZIPファイルを作成
 zip "$zip_file_path" "$file_to_zip"
